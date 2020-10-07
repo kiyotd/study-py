908046cd9b9272ba7f11153f6630c3690705c8c4
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-10-07 18:54:37 +0900

    print

---
+++ src/1_print/main.py
@@ -0,0 +1,18 @@
+# 文字列のプリント
+print('test')  # test
+
+# 変数を使う
+test = 'abc'
+print(test)  # abc
+
+# 数値のプリント
+print(123)  # 123
+
+# 数値の計算
+print(1 + 2)  # 3
+print(3 - 1)  # 2
+print(10 / 2)  # 5.0
+
+# 真偽値の出力
+print(True)  # True
+print(False)  # False
