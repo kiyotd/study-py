14eee80f9534349cf3ad1e80cf718788d54541e4
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-10-08 14:30:16 +0900

    Update 4_if

--- src/4_if/main.py
+++ src/4_if/main.py
@@ -6,3 +6,11 @@ if is_login and type(is_login) == bool:
     print("You are logged in.")
 else:
     print("You are not logged in.")
+
+nums = [0, 1, 2, 3, 4]
+
+# nums に 3 を含むか
+if 3 in nums:
+    print("nums contains 3")
+else:
+    print("not include")
