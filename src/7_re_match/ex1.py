3a4957ffcd9d07128a69015b6ecbac8286e309b5
kiyotd <torigoedesign+kiyotd@gmail.com>
2020-10-19 22:59:22 +0900

    Add 7_re_match

---
+++ src/7_re_match/ex1.py
@@ -0,0 +1,12 @@
+import re
+
+# 全てにマッチ
+if re.match(pattern=r"", string="abcde"):
+    print("match")
+
+# 全てにマッチ
+if re.match(pattern=r"", string="     "):
+    print("match")
+
+c = "aaa" "bbb"
+print(c)
